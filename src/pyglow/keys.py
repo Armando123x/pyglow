40ddae6bd0e8119cf3c363cb4db7f66ebd5a2395
Armando123x <51831060+Armando123x@users.noreply.github.com>
2023-10-04 21:08:24 -0500

    Update keys.py

--- src/pyglow/keys.py
+++ src/pyglow/keys.py
@@ -1,4 +1,4 @@
 
 #actualizar
-PYGLOW_FILES = 'app/others/data_pgw'
-PYGLOW_TABLES = 'app/others/data_pgw/tables_pyglow'
+PYGLOW_FILES = '/app/others/data_pgw'
+PYGLOW_TABLES = '/app/others/data_pgw/tables_pyglow'
